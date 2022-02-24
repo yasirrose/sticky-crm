7f81837060c5b1e8ea0f8c25ea2a1d5dfbe16e49
Yasir <teshuser4321@gmail.com>
2022-02-24 14:21:03 +0500

    Optimization changes and orders modal
    Optimization changes and orders modal

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-
 Auth::routes();
+Route::any('/register', function() {
+    return view('auth.login');
+});
 Route::group(['middleware' => 'auth'], function () {
 Route::any('/{any}', [HomeController::class, 'index'])->where('any', '^(?!api).*$');
 });
