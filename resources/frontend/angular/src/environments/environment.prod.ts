b8b18a131d4b054d147b38815ba3608e212cd894
yasirrose <yasirrose@hotmail.com>
2022-03-16 22:18:59 +0500

    frontend changes
    frontend changes

--- resources/frontend/angular/src/environments/environment.prod.ts
+++ resources/frontend/angular/src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
   googleMapsApiKey: '',
-  // endpoint: 'http://sticky.io:82',
-  endpoint: 'http://sticky.io-test',
-  // endpoint: 'http://ec2-184-72-73-137.compute-1.amazonaws.com',
+  endpoint: 'http://sticky.test',
 };
--- resources/frontend/angular/src/environments/environment.ts
+++ resources/frontend/angular/src/environments/environment.ts
@@ -5,7 +5,5 @@
 
 export const environment = {
   production: false,
-  // endpoint: 'http://sticky.io:82',
-  endpoint: 'http://sticky.io-test',
-  // endpoint: 'http://ec2-184-72-73-137.compute-1.amazonaws.com',
+  endpoint: 'http://sticky.test',
 };
